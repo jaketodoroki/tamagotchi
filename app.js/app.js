1216e7b8971dd340ee561561f1ec09b47c945bc2
JakeTodoroki <taco7897@gmail.com>
2022-12-19 14:06:49 -0700

    healthy qs

--- app.js/app.js
+++ app.js/app.js
@@ -6,3 +6,13 @@ let health = 100
 let happiness = 100
 let hunger = 100
 let timer = 5000 
+
+//event listeners
+
+// funtions 
+
+// query selector 
+//health
+let healthy = document.querySelector('#health');
+healthy.setAttribute('class','health');
+healthy.innerHTML = `Happiness: ${health}`
